2a2ff6535a126c9bd2b1f2496bca928ff368e91f
Abdelhak AMZIL <abdelhakamzil.2000@gmail.com>
2026-05-24 00:45:02 +0100

    create a bank account service

--- src/main/java/com/abdel/bankaccount/entities/BankAccount.java
+++ src/main/java/com/abdel/bankaccount/entities/BankAccount.java
@@ -1,4 +1,24 @@
 package com.abdel.bankaccount.entities;
 
+import com.abdel.bankaccount.enums.AccountType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Builder
 public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private String currency;
+    private AccountType type;
 }
---
+++ src/main/java/com/abdel/bankaccount/repositories/BankAccountRepository.java
@@ -0,0 +1,9 @@
+package com.abdel.bankaccount.repositories;
+
+import com.abdel.bankaccount.entities.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+}
